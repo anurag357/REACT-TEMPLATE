7d7e2c884e859d83a5d2ef5dc601b6c3f4c9876d
anurag357 <pathakanurag753@gmail.com>
2023-01-28 20:12:38 +0530

    inital commit template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import AppSection from "./AppSection";
+import CardSec from "./CardSec";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./Navbar";
+
+const App = () =>{
+    return(
+        <div>
+            <Navbar />
+            <HeroSection />
+            <CardSec />    
+            <AppSection />    
+            <Footer />
+        </div>
+    )
 }
+
 
 export default App;
